dbca9ea44608f6b16f40a7ef75ab21dfcad38d0e
HiroT0905 <163726665+HiroT0905@users.noreply.github.com>
2025-01-01 18:33:06 +0700

    Xử lý Authenticate + Request API Login. Check check SharedPreferences để xem có token không. Nếu có thì vaò trang home, ngược lại đến trang login

---
+++ lib/screen/User/userProfileScreen/userProfile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatelessWidget {
+  const UserProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Thông tin người dùng'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.person, size: 100),
+            SizedBox(height: 16),
+            Text('Tên người dùng: XYZ'),
+            // Thêm các thông tin người dùng khác ở đây
+          ],
+        ),
+      ),
+    );
+  }
+}
