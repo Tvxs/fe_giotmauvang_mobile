ca5ebd80c5347f32fc3af03e8f5ebc9ed56f9122
Tvxs <sonmaket0201@gmail.com>
2025-01-04 00:25:19 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fe_giotmauvang_mobile/screen/User/homeScreen/home.dart';
 import 'package:flutter/material.dart';
 import 'package:fe_giotmauvang_mobile/screen/User/newsScreen/news.dart';
 import 'package:fe_giotmauvang_mobile/screen/User/profile/profile.dart';
+import 'package:fe_giotmauvang_mobile/screen/AppointmentHistoryScreen/AppointmentHistory.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +21,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      home: AppointmentHistoryScreen(),
     );  }
 }
